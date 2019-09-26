2c8888dd3e1ba78bff46622225da39b3b4e0229d
Christian Cuartas <christian.cuartas@gmail.com>
2019-09-25 20:57:02 -0600

    completed crud actions

---
+++ client/src/components/shared/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Header, Container, Image } from 'semantic-ui-react';
+
+const Home = () => (
+  <Container align="center">
+  <Header align="center">Welcome to Blogs</Header>
+  <Image size="large" src="https://images.unsplash.com/photo-1517971071642-34a2d3ecc9cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
+  </Container>
+)
+
+export default Home;
